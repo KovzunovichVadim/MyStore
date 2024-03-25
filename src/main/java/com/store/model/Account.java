bd1e9c9d7873d91f0cffb8142acff9258ae5727a
AkumetsuVadim <akumetsu1988@gmail.com>
2024-03-25 16:06:56 +0300

    47 - CRUD with JPA

--- src/main/java/com/store/model/Account.java
+++ src/main/java/com/store/model/Account.java
@@ -1,5 +1,6 @@
 package com.store.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,11 +9,24 @@ import lombok.RequiredArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "accounts")
 public class Account {
+    @Id
+    @SequenceGenerator(name = "accountSeqGen", sequenceName = "accounts_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "accountSeqGen")
     private Long id;
+
+    @Column(name = "first_name")
     private String firstname;
+
+    @Column(name = "last_name")
     private String lastname;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "created")
+    @Temporal(TemporalType.TIMESTAMP)
     private String created;
 
     public Account(String firstname, String lastname, String email) {
--- src/main/java/com/store/repository/AccountRepository.java
+++ src/main/java/com/store/repository/AccountRepository.java
@@ -1,111 +1,76 @@
 package com.store.repository;
 
 import com.store.model.Account;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AccountRepository {
-    private final String URL = "jdbc:postgresql://localhost:5432/store";
-    private final String USERNAME = "postgres";
-    private final String PASSWORD = "root";
+    private EntityManager entityManager = null;
 
-    private static final String INSERT_USERS_SQL = "INSERT INTO accounts (first_name,last_name,email) VALUES (?,?,?);";
-    private static final String SELECT_ACCOUNT_BY_ID = "SELECT id,first_name,last_name,email,created FROM accounts WHERE id =?;";
-    private static final String SELECT_ALL_ACCOUNTS = "SELECT * FROM accounts";
-    private static final String DELETE_ACCOUNT = "DELETE FROM accounts WHERE id=?;";
-    private static final String UPDATE_ACCOUNT = "UPDATE accounts SET first_name=?,last_name=?,email=? WHERE id=?;";
-
-    public Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e);
-        }
-        return connection;
+    public AccountRepository() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+        entityManager = factory.createEntityManager();
     }
 
-    public void createAccount(Account account) {
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-            preparedStatement.setString(1, account.getFirstname());
-            preparedStatement.setString(2, account.getLastname());
-            preparedStatement.setString(3, account.getEmail());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+    public boolean createAccount(Account account) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(account);
+            entityManager.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
             System.out.println(e);
+            entityManager.getTransaction().rollback();
+            return false;
         }
     }
 
     public Account readAccount(Long id) {
-        Account account = null;
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ACCOUNT_BY_ID)) {
-            preparedStatement.setLong(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
+        try {
+            return entityManager.find(Account.class, id);
 
-            while (resultSet.next()) {
-                String firstname = resultSet.getString("first_name");
-                String lastname = resultSet.getString("last_name");
-                String email = resultSet.getString("email");
-                String created = resultSet.getString("created");
-                account = new Account(id, firstname, lastname, email, created);
-            }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.out.println(e);
+            return null;
         }
-        return account;
     }
 
     public boolean updateAccount(Account account) {
-        boolean rowUpdate;
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_ACCOUNT)) {
-            preparedStatement.setString(1, account.getFirstname());
-            preparedStatement.setString(2, account.getLastname());
-            preparedStatement.setString(3, account.getEmail());
-            preparedStatement.setLong(4, account.getId());
-            rowUpdate = preparedStatement.executeUpdate() > 0;
-            return rowUpdate;
-        } catch (SQLException e) {
+
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(account);
+            entityManager.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
             System.out.println(e);
+            entityManager.getTransaction().rollback();
             return false;
         }
     }
 
     public boolean deleteUser(Long id) {
-        boolean rowDeleted;
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ACCOUNT)) {
-            preparedStatement.setLong(1, id);
-            rowDeleted = preparedStatement.executeUpdate() > 0;
-            return rowDeleted;
-        } catch (SQLException e) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.remove(entityManager.find(Account.class,id));
+            entityManager.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
             System.out.println(e);
+            entityManager.getTransaction().rollback();
             return false;
         }
     }
 
-    public List<Account> readAllAccount(){
+    public List<Account> readAllAccount() {
         List<Account> accounts = new ArrayList<>();
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_ACCOUNTS)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()){
-                Long id = resultSet.getLong("id");
-                String firstname = resultSet.getString("first_name");
-                String lastname = resultSet.getString("last_name");
-                String email = resultSet.getString("email");
-                String created = resultSet.getString("created");
-                accounts.add(new Account(id,firstname,lastname,email,created));
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return accounts;
+        Query query = entityManager.createQuery("FROM accounts ", Account.class);
+        return query.getResultList();
     }
 }
